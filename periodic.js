0312eaa8fba1ffb7c5ad553810d833d89f962e1d
fallax <fallaburr@gmail.com>
2015-01-14 14:57:37 +0000

    Added some test data

---
+++ periodic.js
@@ -0,0 +1,1303 @@
+window.periodic = [ 
+
+{ 
+
+symbol: 'Ac',
+name: 'Actinium',
+etymology: ' from Greek aktinos ',
+number: '89',
+weight: '227',
+group: '0',
+period: '7'
+},
+
+{ 
+
+symbol: 'Ag',
+name: 'Silver',
+etymology: ' Latin argentum ',
+number: '47',
+weight: '107.8682',
+group: '11',
+period: '5'
+},
+
+{ 
+
+symbol: 'Al',
+name: 'Aluminium',
+etymology: ' Latin alumen ',
+number: '13',
+weight: '26.9815386',
+group: '13',
+period: '3'
+},
+
+{ 
+
+symbol: 'Am',
+name: 'Americium',
+etymology: ' the Americas ',
+number: '95',
+weight: '243',
+group: '0',
+period: '7'
+},
+
+{ 
+
+symbol: 'Ar',
+name: 'Argon',
+etymology: ' Greek argon ',
+number: '18',
+weight: '39.948',
+group: '18',
+period: '3'
+},
+
+{ 
+
+symbol: 'As',
+name: 'Arsenic',
+etymology: ' Greek arsenikos ',
+number: '33',
+weight: '74.92160',
+group: '15',
+period: '4'
+},
+
+{ 
+
+symbol: 'At',
+name: 'Astatine',
+etymology: ' Greek astatos ',
+number: '85',
+weight: '210',
+group: '17',
+period: '6'
+},
+
+{ 
+
+symbol: 'Au',
+name: 'Gold',
+etymology: ' Latin aurum ',
+number: '79',
+weight: '196.966569',
+group: '11',
+period: '6'
+},
+
+{ 
+
+symbol: 'B',
+name: 'Boron',
+etymology: ' borax ',
+number: '5',
+weight: '10.811',
+group: '13',
+period: '2'
+},
+
+{ 
+
+symbol: 'Ba',
+name: 'Barium',
+etymology: ' Greek barys ',
+number: '56',
+weight: '137.327',
+group: '2',
+period: '6'
+},
+
+{ 
+
+symbol: 'Be',
+name: 'Beryllium',
+etymology: ' beryl ',
+number: '4',
+weight: '9.012182',
+group: '2',
+period: '2'
+},
+
+{ 
+
+symbol: 'Bh',
+name: 'Bohrium',
+etymology: ' Niels Bohr ',
+number: '107',
+weight: '264',
+group: '7',
+period: '7'
+},
+
+{ 
+
+symbol: 'Bi',
+name: 'Bismuth',
+etymology: ' (New Latin bisemutum from German Wismuth, perhaps from weiße Masse, "white mass") ',
+number: '83',
+weight: '208.98040',
+group: '15',
+period: '6'
+},
+
+{ 
+
+symbol: 'Bk',
+name: 'Berkelium',
+etymology: ' Berkeley, California ',
+number: '97',
+weight: '247',
+group: '0',
+period: '7'
+},
+
+{ 
+
+symbol: 'Br',
+name: 'Bromine',
+etymology: ' Greek bromos ',
+number: '35',
+weight: '79.904',
+group: '17',
+period: '4'
+},
+
+{ 
+
+symbol: 'C',
+name: 'Carbon',
+etymology: ' Latin carbo ',
+number: '6',
+weight: '12.0107',
+group: '14',
+period: '2'
+},
+
+{ 
+
+symbol: 'Ca',
+name: 'Calcium',
+etymology: ' Latin calx ',
+number: '20',
+weight: '40.078',
+group: '2',
+period: '4'
+},
+
+{ 
+
+symbol: 'Cd',
+name: 'Cadmium',
+etymology: ' from Greek kadmia ',
+number: '48',
+weight: '112.411',
+group: '12',
+period: '5'
+},
+
+{ 
+
+symbol: 'Ce',
+name: 'Cerium',
+etymology: ' Ceres ',
+number: '58',
+weight: '140.116',
+group: '0',
+period: '6'
+},
+
+{ 
+
+symbol: 'Cf',
+name: 'Californium',
+etymology: ' State and University of California ',
+number: '98',
+weight: '251',
+group: '0',
+period: '7'
+},
+
+{ 
+
+symbol: 'Cl',
+name: 'Chlorine',
+etymology: ' Greek chloros ',
+number: '17',
+weight: '35.453',
+group: '17',
+period: '3'
+},
+
+{ 
+
+symbol: 'Cm',
+name: 'Curium',
+etymology: ' Marie Curie and the traditional -um ending ',
+number: '96',
+weight: '247',
+group: '0',
+period: '7'
+},
+
+{ 
+
+symbol: 'Co',
+name: 'Cobalt',
+etymology: ' from German kobold ',
+number: '27',
+weight: '58.933195',
+group: '9',
+period: '4'
+},
+
+{ 
+
+symbol: 'Cr',
+name: 'Chromium',
+etymology: ' Greek chroma ',
+number: '24',
+weight: '51.9961',
+group: '6',
+period: '4'
+},
+
+{ 
+
+symbol: 'Cs',
+name: 'Caesium',
+etymology: ' Latin caesius ',
+number: '55',
+weight: '132.9054519',
+group: '1',
+period: '6'
+},
+
+{ 
+
+symbol: 'Cu',
+name: 'Copper',
+etymology: ' Latin Cuprum ',
+number: '29',
+weight: '63.546',
+group: '11',
+period: '4'
+},
+
+{ 
+
+symbol: 'Db',
+name: 'Dubnium',
+etymology: ' Dubna, Russia ',
+number: '105',
+weight: '262',
+group: '5',
+period: '7'
+},
+
+{ 
+
+symbol: 'Ds',
+name: 'Darmstadtium',
+etymology: ' Darmstadt, Germany ',
+number: '110',
+weight: '271',
+group: '10',
+period: '7'
+},
+
+{ 
+
+symbol: 'Dy',
+name: 'Dysprosium',
+etymology: ' Greek dysprositos ',
+number: '66',
+weight: '162.500',
+group: '0',
+period: '6'
+},
+
+{ 
+
+symbol: 'Er',
+name: 'Erbium',
+etymology: ' Ytterby, Sweden ',
+number: '68',
+weight: '167.259',
+group: '0',
+period: '6'
+},
+
+{ 
+
+symbol: 'Es',
+name: 'Einsteinium',
+etymology: ' Albert Einstein ',
+number: '99',
+weight: '252',
+group: '0',
+period: '7'
+},
+
+{ 
+
+symbol: 'Eu',
+name: 'Europium',
+etymology: ' Europe ',
+number: '63',
+weight: '151.964',
+group: '0',
+period: '6'
+},
+
+{ 
+
+symbol: 'F',
+name: 'Fluorine',
+etymology: ' Latin fluo ',
+number: '9',
+weight: '18.9984032',
+group: '17',
+period: '2'
+},
+
+{ 
+
+symbol: 'Fe',
+name: 'Iron',
+etymology: ' Latin ferrum ',
+number: '26',
+weight: '55.845',
+group: '8',
+period: '4'
+},
+
+{ 
+
+symbol: 'Fm',
+name: 'Fermium',
+etymology: ' Enrico Ferm ',
+number: '100',
+weight: '257',
+group: '0',
+period: '7'
+},
+
+{ 
+
+symbol: 'Fr',
+name: 'Francium',
+etymology: ' France ',
+number: '87',
+weight: '223',
+group: '1',
+period: '7'
+},
+
+{ 
+
+symbol: 'Ga',
+name: 'Gallium',
+etymology: ' Latin Gallia ',
+number: '31',
+weight: '69.723',
+group: '13',
+period: '4'
+},
+
+{ 
+
+symbol: 'Gd',
+name: 'Gadolinium',
+etymology: ' gadolinite ',
+number: '64',
+weight: '157.25',
+group: '0',
+period: '6'
+},
+
+{ 
+
+symbol: 'Ge',
+name: 'Germanium',
+etymology: ' Germany ',
+number: '32',
+weight: '72.64',
+group: '14',
+period: '4'
+},
+
+{ 
+
+symbol: 'H',
+name: 'Hydrogen',
+etymology: ' Greek hydror (water) gen (creator) (as makes water when burned in oxygen) ',
+number: '1',
+weight: '1.00794',
+group: '1',
+period: '1',
+info1: 'Rare on earth in elemental form, but most stars are mostly composed of this. Most abundant element in universe',
+info2: 'One of two elements commonly combusted together as rocket fuel',
+info3: 'Normal isotope is the only stable isotope with no neutrons',
+info4: 'When burned in air, water is created; when water is electrolysed, it is created',
+},
+
+{ 
+
+symbol: 'He',
+name: 'Helium',
+etymology: ' Greek helios ("sun", as the element was first noticed in the sun) ',
+number: '2',
+weight: '4.002602',
+group: '18',
+period: '1',
+info1: 'Nucleus is two protons and two neutrons (an alpha particle)',
+info2: 'Lots of helium is found underground in American gas fields; retrieved by fractional distillation of the gas',
+info3: 'Second least reactive element, a Noble gas',
+info4: 'Product of nuclear fusion in stars, second most abundant element in the universe',
+},
+
+{ 
+
+symbol: 'Hf',
+name: 'Hafnium',
+etymology: ' Latin Hafnia ',
+number: '72',
+weight: '178.49',
+group: '4',
+period: '6'
+},
+
+{ 
+
+symbol: 'Hg',
+name: 'Mercury',
+etymology: ' Latin argyrum ',
+number: '80',
+weight: '200.59',
+group: '12',
+period: '6'
+},
+
+{ 
+
+symbol: 'Ho',
+name: 'Holmium',
+etymology: ' Latin Holmia ',
+number: '67',
+weight: '164.930 32',
+group: '0',
+period: '6'
+},
+
+{ 
+
+symbol: 'Hs',
+name: 'Hassium',
+etymology: ' Hesse, Germany ',
+number: '108',
+weight: '277',
+group: '8',
+period: '7'
+},
+
+{ 
+
+symbol: 'I',
+name: 'Iodine',
+etymology: ' Greek ioeides ',
+number: '53',
+weight: '126.904 47',
+group: '17',
+period: '5'
+},
+
+{ 
+
+symbol: 'In',
+name: 'Indium',
+etymology: ' indigo blue ',
+number: '49',
+weight: '114.818',
+group: '13',
+period: '5'
+},
+
+{ 
+
+symbol: 'Ir',
+name: 'Iridium',
+etymology: ' Greek iris ',
+number: '77',
+weight: '192.217',
+group: '9',
+period: '6'
+},
+
+{ 
+
+symbol: 'K',
+name: 'Potassium',
+etymology: ' Latin kalium ',
+number: '19',
+weight: '39.0983',
+group: '1',
+period: '4'
+},
+
+{ 
+
+symbol: 'Kr',
+name: 'Krypton',
+etymology: ' Greek kryptos ',
+number: '36',
+weight: '83.798',
+group: '18',
+period: '4'
+},
+
+{ 
+
+symbol: 'La',
+name: 'Lanthanum',
+etymology: ' Greek lanthanien ',
+number: '57',
+weight: '138.90547',
+group: '0',
+period: '6'
+},
+
+{ 
+
+symbol: 'Li',
+name: 'Lithium',
+etymology: ' Greek "lithos", from the Greek λιθoς (lithos, "stone"), to reflect its discovery in a mineral, as opposed to sodium and potassium which had been discovered in plant tissue ',
+number: '3',
+weight: '6.941',
+group: '1',
+period: '2',
+info1: 'Soft, silvery alkali metal; highly flammable; reacts strongly with water so used as a dessicant',
+info2: 'Stored in oil as it quickly reacts with air and tarnishes',
+info3: 'Very high thermal conductivity so used as a heat transfer material',
+info4: 'Used as an anode, particularly in Lithium ion batteries',
+info5: 'Many medical uses (stabilizes mood, for gout, migraine prevention)',
+},
+
+{ 
+
+symbol: 'Lr',
+name: 'Lawrencium',
+etymology: ' Ernest O. Lawrence ',
+number: '103',
+weight: '262',
+group: '3',
+period: '7'
+},
+
+{ 
+
+symbol: 'Lu',
+name: 'Lutetium',
+etymology: ' Latin Lutetia ',
+number: '71',
+weight: '174.967',
+group: '3',
+period: '6'
+},
+
+{ 
+
+symbol: 'Md',
+name: 'Mendelevium',
+etymology: ' Dmitri Mendeleyev ',
+number: '101',
+weight: '258',
+group: '0',
+period: '7'
+},
+
+{ 
+
+symbol: 'Mg',
+name: 'Magnesium',
+etymology: ' Magnesia, Greece ',
+number: '12',
+weight: '24.3050',
+group: '2',
+period: '3'
+},
+
+{ 
+
+symbol: 'Mn',
+name: 'Manganese',
+etymology: ' Latin magnes ',
+number: '25',
+weight: '54.938045',
+group: '7',
+period: '4'
+},
+
+{ 
+
+symbol: 'Mo',
+name: 'Molybdenum',
+etymology: ' Greek molybdos ',
+number: '42',
+weight: '95.94',
+group: '6',
+period: '5'
+},
+
+{ 
+
+symbol: 'Mt',
+name: 'Meitnerium',
+etymology: ' Lise Meitner ',
+number: '109',
+weight: '268',
+group: '9',
+period: '7'
+},
+
+{ 
+
+symbol: 'N',
+name: 'Nitrogen',
+etymology: ' Greek nitron ',
+number: '7',
+weight: '14.0067',
+group: '15',
+period: '2'
+},
+
+{ 
+
+symbol: 'Na',
+name: 'Sodium',
+etymology: ' Latin natrium ',
+number: '11',
+weight: '22.98976928',
+group: '1',
+period: '3'
+},
+
+{ 
+
+symbol: 'Nb',
+name: 'Niobium',
+etymology: ' Niobe ',
+number: '41',
+weight: '92.906 38',
+group: '5',
+period: '5'
+},
+
+{ 
+
+symbol: 'Nd',
+name: 'Neodymium',
+etymology: ' Greek didymos ',
+number: '60',
+weight: '144.242',
+group: '0',
+period: '6'
+},
+
+{ 
+
+symbol: 'Ne',
+name: 'Neon',
+etymology: ' Greek neos ',
+number: '10',
+weight: '20.1797',
+group: '18',
+period: '2'
+},
+
+{ 
+
+symbol: 'Ni',
+name: 'Nickel',
+etymology: ' German kupfernickel ',
+number: '28',
+weight: '58.6934',
+group: '10',
+period: '4'
+},
+
+{ 
+
+symbol: 'No',
+name: 'Nobelium',
+etymology: ' Alfred Nobel ',
+number: '102',
+weight: '259',
+group: '0',
+period: '7'
+},
+
+{ 
+
+symbol: 'Np',
+name: 'Neptunium',
+etymology: ' Neptune ',
+number: '93',
+weight: '237',
+group: '0',
+period: '7'
+},
+
+{ 
+
+symbol: 'O',
+name: 'Oxygen',
+etymology: ' Greek oxys ',
+number: '8',
+weight: '15.9994',
+group: '16',
+period: '2'
+},
+
+{ 
+
+symbol: 'Os',
+name: 'Osmium',
+etymology: ' Greek osme ',
+number: '76',
+weight: '190.23',
+group: '8',
+period: '6'
+},
+
+{ 
+
+symbol: 'P',
+name: 'Phosphorus',
+etymology: ' Greek phoros ',
+number: '15',
+weight: '30.973762',
+group: '15',
+period: '3'
+},
+
+{ 
+
+symbol: 'Pa',
+name: 'Protactinium',
+etymology: ' Greek actinium ',
+number: '91',
+weight: '231.03588',
+group: '0',
+period: '7'
+},
+
+{ 
+
+symbol: 'Pb',
+name: 'Lead',
+etymology: ' Latin plumbum ',
+number: '82',
+weight: '207.2',
+group: '14',
+period: '6'
+},
+
+{ 
+
+symbol: 'Pd',
+name: 'Palladium',
+etymology: ' Pallas and the traditional -dium ending ',
+number: '46',
+weight: '106.42',
+group: '10',
+period: '5'
+},
+
+{ 
+
+symbol: 'Pm',
+name: 'Promethium',
+etymology: ' Prometheus ',
+number: '61',
+weight: '145',
+group: '0',
+period: '6'
+},
+
+{ 
+
+symbol: 'Po',
+name: 'Polonium',
+etymology: ' Poland ',
+number: '84',
+weight: '210',
+group: '16',
+period: '6'
+},
+
+{ 
+
+symbol: 'Pr',
+name: 'Praseodymium',
+etymology: ' Greek prasios ',
+number: '59',
+weight: '140.90765',
+group: '0',
+period: '6'
+},
+
+{ 
+
+symbol: 'Pt',
+name: 'Platinum',
+etymology: ' Greek ina ',
+number: '78',
+weight: '195.084',
+group: '10',
+period: '6'
+},
+
+{ 
+
+symbol: 'Pu',
+name: 'Plutonium',
+etymology: ' Pluto ',
+number: '94',
+weight: '244',
+group: '0',
+period: '7'
+},
+
+{ 
+
+symbol: 'Ra',
+name: 'Radium',
+etymology: ' Latin radius ',
+number: '88',
+weight: '226',
+group: '2',
+period: '7'
+},
+
+{ 
+
+symbol: 'Rb',
+name: 'Rubidium',
+etymology: ' Latin rubidus ',
+number: '37',
+weight: '85.4678',
+group: '1',
+period: '5'
+},
+
+{ 
+
+symbol: 'Re',
+name: 'Rhenium',
+etymology: ' German Rheinprovinz ',
+number: '75',
+weight: '186.207',
+group: '7',
+period: '6'
+},
+
+{ 
+
+symbol: 'Rf',
+name: 'Rutherfordium',
+etymology: ' Ernest Rutherford ',
+number: '104',
+weight: '261',
+group: '4',
+period: '7'
+},
+
+{ 
+
+symbol: 'Rg',
+name: 'Roentgenium',
+etymology: ' Wilhelm Conrad Röntgen ',
+number: '111',
+weight: '272',
+group: '11',
+period: '7'
+},
+
+{ 
+
+symbol: 'Rh',
+name: 'Rhodium',
+etymology: ' Greek rhodon ',
+number: '45',
+weight: '102.905 50',
+group: '9',
+period: '5'
+},
+
+{ 
+
+symbol: 'Rn',
+name: 'Radon',
+etymology: ' emanation ',
+number: '86',
+weight: '220',
+group: '18',
+period: '6'
+},
+
+{ 
+
+symbol: 'Ru',
+name: 'Ruthenium',
+etymology: ' Latin Ruthenia ',
+number: '44',
+weight: '101.07',
+group: '8',
+period: '5'
+},
+
+{ 
+
+symbol: 'S',
+name: 'Sulphur',
+etymology: ' Latin sulfur ',
+number: '16',
+weight: '32.065',
+group: '16',
+period: '3'
+},
+
+{ 
+
+symbol: 'Sb',
+name: 'Antimony',
+etymology: ' Latin stibium ',
+number: '51',
+weight: '121.760',
+group: '15',
+period: '5'
+},
+
+{ 
+
+symbol: 'Sc',
+name: 'Scandium',
+etymology: ' Scandinavia ',
+number: '21',
+weight: '44.955912',
+group: '3',
+period: '4'
+},
+
+{ 
+
+symbol: 'Se',
+name: 'Selenium',
+etymology: ' Greek selene ',
+number: '34',
+weight: '78.96',
+group: '16',
+period: '4'
+},
+
+{ 
+
+symbol: 'Sg',
+name: 'Seaborgium',
+etymology: ' Glenn T. Seaborg ',
+number: '106',
+weight: '266',
+group: '6',
+period: '7'
+},
+
+{ 
+
+symbol: 'Si',
+name: 'Silicon',
+etymology: ' Latin silex ',
+number: '14',
+weight: '28.0855',
+group: '14',
+period: '3'
+},
+
+{ 
+
+symbol: 'Sm',
+name: 'Samarium',
+etymology: ' samarskite ',
+number: '62',
+weight: '150.36',
+group: '0',
+period: '6'
+},
+
+{ 
+
+symbol: 'Sn',
+name: 'Tin',
+etymology: ' Latin stannum ',
+number: '50',
+weight: '118.710',
+group: '14',
+period: '5'
+},
+
+{ 
+
+symbol: 'Sr',
+name: 'Strontium',
+etymology: ' Latin Strontian ',
+number: '38',
+weight: '87.62',
+group: '2',
+period: '5'
+},
+
+{ 
+
+symbol: 'Ta',
+name: 'Tantalum',
+etymology: ' King Tantalus ',
+number: '73',
+weight: '180.94788',
+group: '5',
+period: '6'
+},
+
+{ 
+
+symbol: 'Tb',
+name: 'Terbium',
+etymology: ' Ytterby, Sweden ',
+number: '65',
+weight: '158.92535',
+group: '0',
+period: '6'
+},
+
+{ 
+
+symbol: 'Tc',
+name: 'Technetium',
+etymology: ' Greek technetos ',
+number: '43',
+weight: '98',
+group: '7',
+period: '5'
+},
+
+{ 
+
+symbol: 'Te',
+name: 'Tellurium',
+etymology: ' Greek tellus ',
+number: '52',
+weight: '127.60',
+group: '16',
+period: '5'
+},
+
+{ 
+
+symbol: 'Th',
+name: 'Thorium',
+etymology: ' Thor ',
+number: '90',
+weight: '232.03806',
+group: '0',
+period: '7'
+},
+
+{ 
+
+symbol: 'Ti',
+name: 'Titanium',
+etymology: ' the Titans ',
+number: '22',
+weight: '47.867',
+group: '4',
+period: '4'
+},
+
+{ 
+
+symbol: 'Tl',
+name: 'Thallium',
+etymology: ' Greek thallos ',
+number: '81',
+weight: '204.3833',
+group: '13',
+period: '6'
+},
+
+{ 
+
+symbol: 'Tm',
+name: 'Thulium',
+etymology: ' Thule and the traditional -ium ending ',
+number: '69',
+weight: '168.93421',
+group: '0',
+period: '6'
+},
+
+{ 
+
+symbol: 'U',
+name: 'Uranium',
+etymology: ' Uranus ',
+number: '92',
+weight: '238.02891',
+group: '0',
+period: '7'
+},
+
+{ 
+
+symbol: 'Uub',
+name: 'Ununbium',
+etymology: ' Latin b ',
+number: '112',
+weight: '285',
+group: '12',
+period: '7'
+},
+
+{ 
+
+symbol: 'Uuh',
+name: 'Ununhexium',
+etymology: ' Latin hex ',
+number: '116',
+weight: '292',
+group: '16',
+period: '7'
+},
+
+{ 
+
+symbol: 'Uuo',
+name: 'Ununoctium',
+etymology: ' Latin oct ',
+number: '118',
+weight: '294',
+group: '18',
+period: '7'
+},
+
+{ 
+
+symbol: 'Uup',
+name: 'Ununpentium',
+etymology: ' Latin pent ',
+number: '115',
+weight: '288',
+group: '15',
+period: '7'
+},
+
+{ 
+
+symbol: 'Uuq',
+name: 'Ununquadium',
+etymology: ' Latin quadr ',
+number: '114',
+weight: '289',
+group: '14',
+period: '7'
+},
+
+{ 
+
+symbol: 'Uut',
+name: 'Ununtrium',
+etymology: ' Latin tri ',
+number: '113',
+weight: '284',
+group: '13',
+period: '7'
+},
+
+{ 
+
+symbol: 'V',
+name: 'Vanadium',
+etymology: ' Vanadis ',
+number: '23',
+weight: '50.9415',
+group: '5',
+period: '4'
+},
+
+{ 
+
+symbol: 'W',
+name: 'Tungsten',
+etymology: ' German fram ',
+number: '74',
+weight: '183.84',
+group: '6',
+period: '6'
+},
+
+{ 
+
+symbol: 'Xe',
+name: 'Xenon',
+etymology: ' Greek xenos ',
+number: '54',
+weight: '131.293',
+group: '18',
+period: '5'
+},
+
+{ 
+
+symbol: 'Y',
+name: 'Yttrium',
+etymology: ' Ytterby, Sweden ',
+number: '39',
+weight: '88.90585',
+group: '3',
+period: '5'
+},
+
+{ 
+
+symbol: 'Yb',
+name: 'Ytterbium',
+etymology: ' Ytterby, Sweden ',
+number: '70',
+weight: '173.04',
+group: '0',
+period: '6'
+},
+
+{ 
+
+symbol: 'Zn',
+name: 'Zinc',
+etymology: ' German zin ',
+number: '30',
+weight: '65.409',
+group: '12',
+period: '4'
+},
+
+{ 
+
+symbol: 'Zr',
+name: 'Zirconium',
+etymology: ' zircon ',
+number: '40',
+weight: '91.224',
+group: '4',
+period: '5'
+}
+
+]
